c97cc5f1ba260f4f5493f227dc10e2074420f6ac
0l9h <olehbobeliak@gmail.com>
2020-04-17 23:43:48 +0300

    Added another directory to archive

--- backup_ver4.py
+++ backup_ver4.py
@@ -2,16 +2,27 @@ import os
 import time
 import zipfile
 
+current_time = time.strftime('%Y%m%d%H%M%S')    #   current time
+print(current_time)
 
+folderPath1 = 'C:\\My Documents'
+folderPath2 = 'C:\\Code'    #   Path to the folder, which you need to archive
 
-folderPath = 'C:\\My Documents'     #   Путь к папке, которую нужно архивировать
-archievePath = 'D:\\Python\\backup\\test10.zip'
+archivePath = 'D:\\Python\\backup\\'+current_time+'.zip'
 
 
 # create a ZipFile object
-with zipfile.ZipFile(archievePath, 'w') as zipObj:
-   # Iterate over all the files in directory
-   for folderName, subfolders, filenames in os.walk(folderPath):
+with zipfile.ZipFile(archivePath, 'w') as zipObj:
+   # Iterate over all the files in directory 1
+   for folderName, subfolders, filenames in os.walk(folderPath1):
+       for filename in filenames:
+           #create complete filepath of file in directory
+           filePath = os.path.join(folderName, filename)
+           # Add file to zip
+           zipObj.write(filePath)
+    
+       # Iterate over all the files in directory 2
+   for folderName, subfolders, filenames in os.walk(folderPath2):
        for filename in filenames:
            #create complete filepath of file in directory
            filePath = os.path.join(folderName, filename)
