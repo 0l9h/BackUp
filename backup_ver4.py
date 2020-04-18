f68093d8d77cbd1195d84e150be6e971516a7f26
0l9h <olehbobeliak@gmail.com>
2020-04-18 22:14:21 +0300

    Created function to parse through all the directory

--- backup_ver4.py
+++ backup_ver4.py
@@ -7,24 +7,21 @@ print(current_time)
 
 folderPath1 = 'C:\\My Documents'     #   Path to the folder, which you need to archive
 folderPath2 = 'C:\\Code'
+
 archivePath = 'D:\\Python\\backup\\'+current_time+'.zip'
+
+
+def Parser(folderPath):
+       # Iterate over all the files in directory 1
+   for folderName, subfolders, filenames in os.walk(folderPath):
+       for filename in filenames:
+           #create complete filepath of file in directory
+           filePath = os.path.join(folderName, filename)
+           # Add file to zip
+           zipObj.write(filePath)
 
 
 # create a ZipFile object
 with zipfile.ZipFile(archivePath, 'w') as zipObj:
-
-   # Iterate over all the files in directory 1
-   for folderName, subfolders, filenames in os.walk(folderPath1):
-       for filename in filenames:
-           #create complete filepath of file in directory
-           filePath = os.path.join(folderName, filename)
-           # Add file to zip
-           zipObj.write(filePath)
-    
-       # Iterate over all the files in directory 2
-   for folderName, subfolders, filenames in os.walk(folderPath2):
-       for filename in filenames:
-           #create complete filepath of file in directory
-           filePath = os.path.join(folderName, filename)
-           # Add file to zip
-           zipObj.write(filePath)
+    Parser(folderPath1)
+    Parser(folderPath2)
